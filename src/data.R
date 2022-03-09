a05c86fccb002e9cca3475f809484644fd0af461
Sean Casey <sccasey04@gmail.com>
2022-03-09 15:42:18 -0800

    Begin working on data file

--- src/data.R
+++ src/data.R
@@ -1,2 +1,14 @@
 library(tidyverse)
+library(here)
+library(readxl)
+
+get_raw_data <- function() {
+    data_file <- file.path(here(), "data", "WindTurbineDatabase.xlsx")
+    data <- read_excel(data_file)
+    
+    return(data)
+}
+
+
+
 
