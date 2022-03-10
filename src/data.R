112f55c99064123c2d2a13fbdba05ce806e17975
Sean Casey <sccasey04@gmail.com>
2022-03-09 19:36:43 -0800

    Add common data reading and cleaning functions

--- src/data.R
+++ src/data.R
@@ -9,6 +9,18 @@ get_raw_data <- function() {
     return(data)
 }
 
-
-
-
+get_data <- function() {
+    data <- get_raw_data()
+    
+    # Keep only the first year when multiple are given for "Comissioning date"
+    # and convert from string to integer
+    data <- data |> 
+        separate(
+            `Commissioning date`, 
+            "Commissioning date", 
+            extra = "drop"
+        ) |> 
+        mutate(`Commissioning date` = as.integer(`Commissioning date`)) 
+    
+    return(data)
+}
