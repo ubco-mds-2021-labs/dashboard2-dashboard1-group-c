c23a6e5630ffec5ae149d07a28bb74853f881909
Sean Casey <sccasey04@gmail.com>
2022-03-09 19:45:03 -0800

    Arrange import/source statements

--- src/app.R
+++ src/app.R
@@ -1,5 +1,9 @@
 library(dash)
 source("src/layout.R")
+source("src/map.R")
+source("src/models.R")
+source("src/time_count.R")
+source("src/total_capacity.R")
 
 app <- dash_app()
 app |> set_layout(
