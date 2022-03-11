31773d52ad6593a4d834bce178c7b1239bc47c1c
Sean Casey <sccasey04@gmail.com>
2022-03-11 12:22:19 -0800

    Write function to get cumulative counts

--- src/time_count.R
+++ src/time_count.R
@@ -1,1 +1,25 @@
 source("src/data.R")
+
+
+time_count <- function(prov = NULL) {
+    data <- get_data()
+    
+    if (! is.null(prov))
+        data <- data |> filter(`Province/Territory` == prov)
+    
+    data <- data |> count(`Commissioning date`)
+    
+    current_total <- 0
+    counts <- c()
+    years <- c()
+    for (i in 1:nrow(data)) {
+        year <- test[[i, 1]]
+        n <- test[[i, 2]]
+        
+        years <- c(years, year)
+        current_total <- current_total + n
+        counts <- c(counts, current_total)
+    }
+    
+    return(tibble(year = years, count = counts))
+}
