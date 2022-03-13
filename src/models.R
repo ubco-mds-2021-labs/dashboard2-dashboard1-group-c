bbe13415b5f4d8e9b2508352e9ede81bb221868c
TingwenH <tingwenh@student.ubc.ca>
2022-03-13 13:02:55 +0800

    Add viz

--- src/models.R
+++ src/models.R
@@ -1,1 +1,28 @@
 source("src/data.R")
+library(tidyverse) 
+library(plotly)
+library(ggplot2)
+
+wind <- get_data()
+
+plot_model <- function(prov){
+  if(is.null(prov)){
+    prov="Alberta"
+  }
+  data <- wind[wind$'Province/Territory' == prov,]
+  model_province <- data %>%
+    group_by(Model) %>%
+    summarise(count= n())
+  plot <- ggplot(model_province,aes(x = '',y=count, fill= Model)) + 
+                   geom_bar(stat="identity", width=1) +
+                   coord_polar("y", start=0) +
+                   theme_void() 
+                   
+  
+  return(ggplotly(plot))
+}
+
+model_viz <- plot_model(prov = NULL)
+model_province <- dccGraph(id='ggplot_model_count',figure =model_viz)
+
+  
